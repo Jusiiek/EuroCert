393ab5460e1ead41adaf79fd0acbb4fec213a122
Jusiiek <jakub.zurawski@outlook.com>
2025-08-09 18:48:17 +0200

    Added user manager, #1

--- api/euro_cert_api/models/base.py
+++ api/euro_cert_api/models/base.py
@@ -1,13 +1,10 @@
-from pydantic import BaseModel, Field
 from bson import ObjectId
+from beanie import Document
+from euro_cert_api.utils.models import CreateUpdateDictModel
 
-from euro_cert_api.models import PyObjectId
 
-
-class Base(BaseModel):
-    id: PyObjectId = Field(default_factory=PyObjectId, alias="_id")
+class Base(Document, CreateUpdateDictModel):
 
     class Config:
         populate_defaults = True
-        arbitrary_types_allowed = True
         json_loads = {ObjectId: str}
--- api/euro_cert_api/models/user.py
+++ api/euro_cert_api/models/user.py
@@ -1,8 +1,19 @@
 from euro_cert_api.models.base import Base
-from euro_cert_api.utils.models import CreateUpdateDictModel
 
 
-class User(CreateUpdateDictModel, Base):
+class User(Base):
     email: str
     hashed_password: str
     is_active: bool = True
+
+    class Settings:
+        name = "users"
+
+
+    @classmethod
+    async def get_by_id(cls, user_id):
+        return await cls.filter(id=user_id).first()
+
+    @classmethod
+    async def get_by_email(cls, email):
+        return await cls.filter(email=email).first()
